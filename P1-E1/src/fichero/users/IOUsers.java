7ad7f9aa1e84900a7b6124a0bf9796df603f3f6d
Antonio Lujano Luna <71549483+AntonioLujanoLuna@users.noreply.github.com>
2021-09-30 12:28:31 +0200

    Cambio minimo
    La idea para IO está pensada, complicada implementación. Falta además comprobar cuando vas a escribir que si ya existe ese elem no lo vuelvas a escribir. Al final la implementación de Ricardo será lo más fácil.

--- P1-E1/src/fichero/users/IOUsers.java
+++ P1-E1/src/fichero/users/IOUsers.java
@@ -19,16 +19,28 @@ public class IOUsers {
 		String rutaAbsoluta = new File("").getAbsolutePath();
 		String rutaFicheroPropiedades = rutaAbsoluta + "/usuarios.properties";
 		try {
-		InputStream entrada = new FileInputStream(rutaFicheroPropiedades); 
-		//ABRIMOS EL FICHERO
-		propiedades.load(entrada);
-		//RELLENAMOS CON LOS NUEVOS DATOS
-		propiedades.setProperty("name", name);
-		propiedades.setProperty("mail", mail); 
-		propiedades.setProperty("username", username); 
-
-		//CARGAMOS LOS DATOS AL FICHERO
-		propiedades.store(new FileWriter(rutaFicheroPropiedades), "");
+			InputStream entrada = new FileInputStream(rutaFicheroPropiedades); 
+			//ABRIMOS EL FICHERO
+	
+			propiedades.load(entrada);
+			//RELLENAMOS CON LOS NUEVOS DATOS
+			int elems = propiedades.size();
+			if (elems==0) {
+				propiedades.setProperty("name"+1, name);
+				propiedades.setProperty("mail"+1, mail); 
+				propiedades.setProperty("username"+1, username);
+				propiedades.store(new FileWriter(rutaFicheroPropiedades), "");
+			}
+			else {
+				for(int i=1; i<elems;i++) {
+					propiedades.setProperty("name"+i, name);
+					propiedades.setProperty("mail"+i, mail); 
+					propiedades.setProperty("username"+i, username); 
+					//CARGAMOS LOS DATOS AL FICHERO
+					propiedades.store(new FileWriter(rutaFicheroPropiedades), "");
+				}
+			}
+			
 
 			}catch(FileNotFoundException e) {
 			e.printStackTrace();
--- P1-E1/src/main/program/mainProgram.java
+++ P1-E1/src/main/program/mainProgram.java
@@ -4,10 +4,8 @@ public class mainProgram {
 
 	public static void main(String[] args) 
 	{
-		Espectador e1; 
-		e1 = new Espectador("RodrigoRato", "RodrigoRato@hotmail.com","rodrigo1234"); 
-		Espectador e2; 
-		e2 = new Espectador("fdfsfs", "RodrigoRato@hotmail.com","rodrigo1234");
+		new Espectador("RodrigoRato", "RodrigoRato@hotmail.com","rodrigo1234"); 
+		new Espectador("fdfsfs", "RodrigoRato@hotmail.com","rodrigo1234");
 	}
 
 }
