fcab9c0ffb64d23a2fb401903553e533f19ad768
Antonio Lujano Luna <71549483+AntonioLujanoLuna@users.noreply.github.com>
2021-10-04 18:08:20 +0200

    UPDATE
    updateo

--- P1-E1/src/credenciales/Credenciales.java
+++ P1-E1/src/credenciales/Credenciales.java
@@ -1,24 +1,70 @@
 package credenciales;
-
+/**
+ * La clase credenciales comprende las variables privadas usuario y passwd
+ * necesarias para ejecutar el sistema y para que funcione correctamente.
+ * <p> Además cuenta con dos constructores diferentes y los getters y 
+ * setters correspondientes
+ * @author Antonio Lujano Luna
+ * 
+ */
 public class Credenciales {
+	/**
+	 * Cadena User para almacenar el usuario registrado
+	 */
 	private String User;
+	
+	/**
+	 * Cadena password asociada al user, se almacena en texto plano
+	 */
 	private String Passwd;
 	
+	/**
+	 * Constructor parametrizado
+	 * @param User el usuario con el que queremos trabajar 
+	 * @param Passwd la contraseña del usuario
+	 */
 	public Credenciales(String User, String Passwd) {
 		this.User = User;
 		this.Passwd = Passwd;
 	}
+	
+	/**
+	 * Constructor sin parámetros. Usado para inicializar variables
+	 * antes de contar con los datos con los que queremos inicializar.
+	 */
 	public Credenciales() {
 	}
+	
+	/**
+	 * Setter de la cadena del user
+	 * @param User Username
+	 */
 	public void setUser(String User) {
 		this.User = User;
 	}
+	
+	/**
+	 * Setter de la contraseña
+	 * @param Passwd Contraseña del user
+	 */
 	public void setPasswd(String Passwd) {
 		this.Passwd = Passwd;
 	}
+	
+	/**
+	 * Getter de la cadena del user
+	 * @return Cadena que contiene el username
+	 */
 	public String getUser() {
 		return this.User;
 	}
+	
+	/**
+	 * Getter de la contraseña. Usado para hacer login. 
+	 * No es seguro que sea public y la contraseña 
+	 * esté almacenada en texto plano.
+	 * @return Cadena que contiene la contraseña
+	 */
 	public String getPasswd() {
 		return this.Passwd;
 	}
--- P1-E1/src/espectador/Espectador.java
+++ P1-E1/src/espectador/Espectador.java
@@ -1,20 +1,40 @@
 package espectador;
 import fichero.users.IOUsers;
 
+/**
+ * Clase que implementa un espectador con sus variables privadas 
+ * y metodos
+ * @author Antonio Lujano Luna
+ *
+ */
 public class Espectador extends IOUsers{
 
+	/**
+	 * Cadena que almacena el nombre del usuario
+	 */
 	private String name; 
+	/**
+	 * Cadena que almacena el mail del usuario
+	 */
 	private String mail;
+	/**
+	 * Cadena que almacena el username del usuario
+	 */
 	private String username; 
 	
-	//Constructor parametrizado
-	
-	//constructor vacio
+	/**
+	 * Constructor sin parametros usado en inicializacion
+	 * de variables
+	 */
 	public Espectador()
 	{
-		
 	}
-	
+	/**
+	 * Constructor parametrizado.
+	 * @param name Nombre del usuario
+	 * @param mail Mail del usuario
+	 * @param username Username del usuario
+	 */
 	public Espectador(String name, String mail, String username)
 	{
 		this.name=name; 
@@ -23,33 +43,54 @@ public class Espectador extends IOUsers{
 		userToFich(this.name, this.mail, this.username); 
 	}
 	//OBSERVADORES
-	
+	/**
+	 * Getter del nombre del usuario
+	 * @return Cadena con el nombre del usuario
+	 */
 	public String getName()
 	{
 		return this.name; 
 	}
+	/**
+	 * Getter del mail del usuario
+	 * @return Cadena con el mail del usuario
+	 */
 	public String getMail()
 	{
 		return this.mail; 
 	}
+	/**
+	 * Getter del username del usuario
+	 * @return Cadena con el username del usuario
+	 */
 	public String getUsername()
 	{
 		return this.username; 
 	}
 	
 	//MODIFICADORES
+	/**
+	 * Setter del nombre del usuario
+	 * @param name Nombre del usuario
+	 */
 	public void setName(String name)
 	{
 		this.name = name; 
 	}
+	/**
+	 * Setter del mail del usuario
+	 * @param mail Mail del usuario
+	 */
 	public void setMail(String mail)
 	{
 		this.mail = mail; 
 	}
+	/**
+	 * Setter del username del usuario
+	 * @param username Username del usuario
+	 */
 	public void setUsername(String username)
 	{
 		this.username = username; 
 	}	
-	
-	
 }
