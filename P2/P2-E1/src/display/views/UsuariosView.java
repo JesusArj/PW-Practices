e46c066cae8d220a15f992edcc9f79bd979c77ab
Valentín <valentingabrielavram@gmail.com>
2021-11-04 17:42:08 +0000

    Add files via upload

--- P2/P2-E1/src/display/views/UsuariosView.java
+++ P2/P2-E1/src/display/views/UsuariosView.java
@@ -3,6 +3,7 @@ package display.views;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 /**
  * La clase MenuUsuario contiene la funcion UserMenu al usuario.
  * @author Developers
@@ -33,16 +34,45 @@ public class UsuariosView
         
         if("1".equals(opc)) 
 		{ 
-        	//TODO
+        	System.out.println("DATOS DE USUARIO");
+        	//TODO: LO QUE DEVUELVA EL DTO
         }
         else if("2".equals(opc))
         { 
-			
-        	//TODO
+        	System.out.println("ACTUALIZACION DE DATOS");
+        	System.out.println("IMPORTANTE: Si hay un valor que NO desee actualizar, escriba su valor actual");
+        	String username = null;
+        	Scanner username_scan = new Scanner(System.in);
         	
+        	String email = null;
+        	Scanner mail_scan = new Scanner(System.in);
+        	
+        	String name = null;
+        	Scanner name_scan = new Scanner(System.in);
+        	
+        	String password = null;
+        	Scanner password_scan = new Scanner(System.in);
+        	
+        	System.out.println("Introduzca su nuevo nombre de usuario");
+        	username = username_scan.nextLine();
+        	
+        	System.out.println("Introduzca su nuevo mail");
+        	username = mail_scan.nextLine();
+        	
+        	System.out.println("Introduzca su nuevo nombre");
+        	name = name_scan.nextLine();
+        	
+        	System.out.println("Introduzca su nueva Password");
+        	password = password_scan.nextLine();
+        	
+        	//TODO: Usar las funcione del DTO
+        	username_scan.close();
+        	name_scan.close();
+        	password_scan.close();
         }
         else if("3".equals(opc))
         { 
+        	System.out.println("ELMINACION DE USUARIO");
         	//TODO
         }
         else
