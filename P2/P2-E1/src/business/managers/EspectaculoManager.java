bf5840b74ec8130292a3e84f082b3fb3fd89ce87
Valentín <valentingabrielavram@gmail.com>
2021-11-07 18:22:57 +0000

    Add files via upload

--- P2/P2-E1/src/business/managers/EspectaculoManager.java
+++ P2/P2-E1/src/business/managers/EspectaculoManager.java
@@ -1,5 +1,195 @@
 package business.managers;
 
-public class EspectaculoManager {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import business.DTOs.EspectaculoMultDTO;
+import business.DTOs.EspectaculoPuntDTO;
+import business.DTOs.EspectaculoTempDTO;
+import business.DTOs.FechasDTO;
+import business.DTOs.PasesDTO;
+import data.DAOs.EspectaculoDAO;
 
+public class EspectaculoManager
+{
+	
+	//TODO: Hacer Funciones PuntExist(), MultExist(), TempExist(). Comprobar si ID existe
+
+	public Boolean PuntExist(int id) {
+		ArrayList<EspectaculoPuntDTO> Punt = this.requestEPs();
+		for(EspectaculoPuntDTO u : Punt) {
+			if(u.getID() == id) {
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public Boolean MultExist(int id) {
+		ArrayList<EspectaculoMultDTO> Mult = this.requestEMs();
+		for(EspectaculoMultDTO u : Mult) {
+			if(u.getID() == id) {
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public Boolean TempExist(int id) {
+		ArrayList<EspectaculoTempDTO> Criticas = this.requestETs();
+		for(EspectaculoTempDTO u : Criticas) {
+			if(u.getID() == id) 
+			{
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public Boolean createEspectaculoPunt(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas,LocalDateTime fecha) {
+		EspectaculoDAO newPuntDAO = new EspectaculoDAO();
+		//TODO funcion id;
+		if(!this.PuntExist(id)) 
+		{
+			EspectaculoPuntDTO newPuntDTO = new EspectaculoPuntDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, fecha);  
+			newPuntDAO.createEspectaculoPuntual(newPuntDTO);	
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean updateEspectaculoPunt(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas,LocalDateTime fecha) {
+			if(this.PuntExist(id)) 
+			{
+				EspectaculoDAO newEspectaculo = new EspectaculoDAO();			
+				EspectaculoPuntDTO newPuntDTO = new EspectaculoPuntDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, fecha);  
+				newEspectaculo.updateEspectaculoPuntual(newPuntDTO);
+				return true;		
+		}
+		return false;
+	}
+	
+	public Boolean deleteEspectaculoPunt(int id) {
+		if(this.PuntExist(id)) 
+		{
+			EspectaculoDAO deletePunt = new EspectaculoDAO();
+			deletePunt.deleteEspectaculoPuntual(id);
+			return true;				
+		}	
+		return false;
+	}
+	
+	public EspectaculoPuntDTO requestEspectaculoPunt(int id) 
+	{
+		EspectaculoDAO requestPunt = new EspectaculoDAO();
+		EspectaculoPuntDTO requestedPunt = new EspectaculoPuntDTO();
+		requestedPunt = requestPunt.requestEspectaculoPuntual(id);
+		return requestedPunt;
+	}
+	
+	public ArrayList<EspectaculoPuntDTO> requestEPs()
+	{
+		EspectaculoDAO requestPunt = new EspectaculoDAO();
+		ArrayList<EspectaculoPuntDTO> requestedPunt = new ArrayList<EspectaculoPuntDTO>();
+		requestedPunt = requestPunt.requestEPs();
+		return requestedPunt;
+	}
+	
+	public Boolean createEspectaculoMult(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas, ArrayList<FechasDTO> fechas) {
+		EspectaculoDAO newMultDAO = new EspectaculoDAO();
+		//TODO funcion id;
+		if(!this.MultExist(id)) 
+		{
+			EspectaculoMultDTO newMultDTO = new EspectaculoMultDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, fechas);  
+			newMultDAO.createEspectaculoMultiple(newMultDTO);	
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean updateEspectaculoMultiple(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas,ArrayList<FechasDTO> fechas) {
+			if(this.MultExist(id)) 
+			{
+				EspectaculoDAO newEspectaculo = new EspectaculoDAO();			
+				EspectaculoMultDTO newMultDTO = new EspectaculoMultDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, fechas);  
+				newEspectaculo.updateEspectaculoMultiple(newMultDTO);
+				return true;		
+		}
+		return false;
+	}
+	
+	public Boolean deleteEspectaculoMultiple(int id) {
+		if(this.MultExist(id)) 
+		{
+			EspectaculoDAO deleteMult = new EspectaculoDAO();
+			deleteMult.deleteEspectaculoMultiple(id);
+			return true;				
+		}	
+		return false;
+	}
+	
+	public EspectaculoMultDTO requestEspectaculoMultiple(int id) 
+	{
+		EspectaculoDAO requestMult = new EspectaculoDAO();
+		EspectaculoMultDTO requestedMult = new EspectaculoMultDTO();
+		requestedMult = requestMult.requestEspectaculoMultiple(id);
+		return requestedMult;
+	}
+	
+	public ArrayList<EspectaculoMultDTO> requestEMs()
+	{
+		EspectaculoDAO requestMult = new EspectaculoDAO();
+		ArrayList<EspectaculoMultDTO> requestedMult = new ArrayList<EspectaculoMultDTO>();
+		requestedMult = requestMult.requestEMs();
+		return requestedMult;
+	}
+	
+	public Boolean createEspectaculoTemp(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas, ArrayList<PasesDTO> pases) {
+		EspectaculoDAO newTempDAO = new EspectaculoDAO();
+		//TODO funcion id;
+		if(!this.MultExist(id)) 
+		{
+			EspectaculoTempDTO newTempDTO = new EspectaculoTempDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, pases);  
+			newTempDAO.createEspectaculoTemporada(newTempDTO);	
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean updateEspectaculoTemp(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas,ArrayList<PasesDTO> pases) {
+			if(this.TempExist(id)) 
+			{
+				EspectaculoDAO newEspectaculo = new EspectaculoDAO();			
+				EspectaculoTempDTO newTempDTO = new EspectaculoTempDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, pases);  
+				newEspectaculo.updateEspectaculoTemporada(newTempDTO);
+				return true;		
+		}
+		return false;
+	}
+	
+	public Boolean deleteEspectaculoTemp(int id) {
+		if(this.PuntExist(id)) 
+		{
+			EspectaculoDAO deleteTemp = new EspectaculoDAO();
+			deleteTemp.deleteEspectaculoTemporada(id);
+			return true;				
+		}	
+		return false;
+	}
+	
+	public EspectaculoTempDTO requestEspectaculoTemporada(int id) 
+	{
+		EspectaculoDAO requestTemp = new EspectaculoDAO();
+		EspectaculoTempDTO requestedTemp = new EspectaculoTempDTO();
+		requestedTemp = requestTemp.requestEspectaculoTemporada(id);
+		return requestedTemp;
+	}
+	
+	public ArrayList<EspectaculoTempDTO> requestETs()
+	{
+		EspectaculoDAO requestTemp = new EspectaculoDAO();
+		ArrayList<EspectaculoTempDTO> requestedTemp = new ArrayList<EspectaculoTempDTO>();
+		requestedTemp = requestTemp.requestETs();
+		return requestedTemp;
+	}
+	
 }
