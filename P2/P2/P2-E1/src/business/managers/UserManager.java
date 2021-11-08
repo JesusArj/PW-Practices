2f771aa1f998c28bf5d9da783e426965f0cd1271
Jesús Arjona <71668033+JesusArj@users.noreply.github.com>
2021-11-08 14:27:07 +0100

    avance criticasview

---
+++ P2/P2/P2-E1/src/business/managers/UserManager.java
@@ -0,0 +1,105 @@
+package business.managers;
+
+import java.util.ArrayList;
+
+import business.DTOs.UserDTO;
+import data.DAOs.UserDAO;
+
+public class UserManager {
+	
+	public Boolean login(String mail, String passwd) {
+		if(this.UserExist(mail)) {
+			UserDAO login = new UserDAO();
+			String password = login.requestCredenciales(mail);
+			if(passwd.equals(password)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Boolean createUser(String mail, String username, String name, String passwd) {	
+		if(!this.UserExist(mail)) {
+			UserDAO newUser = new UserDAO();
+			UserDTO newUserDTO = new UserDTO(name,mail,username,passwd);  
+			newUser.createUser(newUserDTO);
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean updateUser(String mail, String username, String name, String passwd) {
+		if(this.UserExist(mail)) {
+			UserDAO newUser = new UserDAO();
+			UserDTO newUserDTO = new UserDTO(name,mail,username,passwd);  
+			newUser.updateUser(newUserDTO);
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean deleteUser(String mail) {
+		if(this.UserExist(mail)) {
+			UserDAO deleteUser = new UserDAO();
+			deleteUser.deleteUser(mail);
+			return true;
+		}	
+		return false;
+	}
+	
+	public UserDTO requestUser(String mail) {
+		UserDAO requestUser = new UserDAO();
+		UserDTO requestedUser = new UserDTO();
+		if(this.UserExist(mail)) {
+			requestedUser = requestUser.requestUser(mail);	
+		}
+		return requestedUser;
+	}
+	
+	public UserDTO requestUserByUsername(String username) {
+		UserDAO requestUser = new UserDAO();
+		UserDTO requestedUser = new UserDTO();
+		if(this.UserExistByUsername(username)) {
+			requestedUser = requestUser.requestUserByUsername(username);	
+		}
+		return requestedUser;
+	}
+	
+	public ArrayList<UserDTO> requestUsers(){
+		UserDAO requestUser = new UserDAO();
+		ArrayList<UserDTO> requestedUsers = new ArrayList<UserDTO>();
+		requestedUsers = requestUser.requestUsers();
+		return requestedUsers;
+	}
+	
+	public Boolean UserExist(String mail) {
+		ArrayList<UserDTO> users = this.requestUsers();
+		for(UserDTO u : users) {
+			if(u.getMail().equals(mail)) {
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public Boolean UserExistByUsername(String username) {
+		ArrayList<UserDTO> users = this.requestUsers();
+		for(UserDTO u : users) {
+			if(u.getUsername().equals(username)) {
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public ArrayList<String> readUser(String mail){
+		ArrayList<String> userS =  new ArrayList<String>();
+		if(this.UserExist(mail)) {
+			UserDTO user = this.requestUser(mail);
+			userS.add(user.getMail());
+			userS.add(user.getName());
+			userS.add(user.getUsername());	
+		}
+		return userS;
+	}
+}
---
+++ P2/P2/P2-E1/src/display/views/UsuariosView.java
@@ -0,0 +1,118 @@
+package display.views;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import business.managers.UserManager;
+/**
+ * La clase MenuUsuario contiene la funcion UserMenu al usuario.
+ * @author Developers
+ */
+
+public class UsuariosView 
+{
+	private String mail;
+	
+	private String getMail() {
+		return mail;
+	}
+	
+	public UsuariosView(String mail) {
+		this.mail = mail;
+	}
+	
+	public void userMenu() 
+	{
+		String opc = "1";
+		while(opc.equals("1") || opc.equals("2") || opc.equals("3")) {
+			System.out.println("Bienvenido a nuestro Menu de gestion de usuarios.");
+		    System.out.println("Para ver los datos de su perfil, pulse 1.");
+		    System.out.println("Para actualizar sus datos de usuario, pulse 2.");
+		    System.out.println("Para dar de baja su usuario, pulse 3.");	    
+		    System.out.println("Para salir del menu, pulse cualquier otra tecla.");
+		    
+	        BufferedReader login = new BufferedReader(new InputStreamReader(System.in));
+	        try 
+	        {
+				opc = login.readLine();
+			} 
+	        catch (IOException e) 
+	        {
+				e.printStackTrace();
+			}
+	        
+	        if("1".equals(opc)) 
+			{ 
+	        	System.out.println("DATOS DE USUARIO");
+	        	UserManager managerUserRead = new UserManager();
+	        	ArrayList<String> user = managerUserRead.readUser(this.getMail());
+	        	System.out.println("Mail : " + user.get(2));
+	        	System.out.println("Name : " + user.get(1));
+	        	System.out.println("Username : " + user.get(2));  	
+	        }
+	        else if("2".equals(opc))
+	        { 
+	        	System.out.println("ACTUALIZACION DE DATOS");
+	        	System.out.println("Estos son sus datos actuales:");
+	        	UserManager managerUser = new UserManager();
+	        	ArrayList<String> user = managerUser.readUser(this.getMail());
+	        	System.out.println("	Mail : " + user.get(0));
+	        	System.out.println("	Name : " + user.get(1));
+	        	System.out.println("	Username : " + user.get(2)); 
+	        	System.out.println("IMPORTANTE: Si hay un valor que NO desee actualizar, escriba su valor actual");
+	        	String username = null;
+	        	Scanner username_scan = new Scanner(System.in);
+	        	
+	        	String name = null;
+	        	Scanner name_scan = new Scanner(System.in);
+	        	
+	        	String password = null;
+	        	Scanner password_scan = new Scanner(System.in);
+	        	
+	        	System.out.println("Introduzca su nuevo nombre de usuario");
+	        	username = username_scan.nextLine();
+	        	
+	        	System.out.println("Introduzca su nuevo nombre");
+	        	name = name_scan.nextLine();
+	        	
+	        	System.out.println("Introduzca su nueva Password");
+	        	password = password_scan.nextLine();
+
+	        	UserManager managerUserUpdate = new UserManager();
+	        	managerUserUpdate.updateUser(this.getMail(), username, name, password);
+	        	
+	        	username_scan.close();
+	        	name_scan.close();
+	        	password_scan.close();
+	        }
+	        else if("3".equals(opc))
+	        { 
+	        	System.out.println("ELMINACION DE USUARIO");
+	        	System.out.println("Esta accion es permanente. ¿Desea borrar su usuario? Y / N.");
+	        	Scanner delete_scan = new Scanner(System.in);
+	        	String delete = delete_scan.nextLine();
+	        	if(delete.equals("Y")) {
+	        		UserManager managerUserDelete = new UserManager();
+	        		managerUserDelete.deleteUser(this.getMail());
+	        		System.out.println("Su usuario ha sido borrado.");
+	        		System.out.println("Gracias por usar nuestro sistema.");
+	        		System.exit(0);
+		    	}
+	        	else {
+	        		System.out.println("Su usuario no ha sido borrado.");	
+	        	}
+	        	delete_scan.close();
+	        }
+	        else
+	        { 
+	        	System.out.println("Saliendo...\n\n");
+	        }
+
+			
+			
+		}
+	}
+}
