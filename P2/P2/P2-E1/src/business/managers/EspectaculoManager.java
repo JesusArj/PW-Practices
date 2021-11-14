4285f0ca9ab016de69650cf2ca7c19b11a4e2254
Jesús Arjona <71668033+JesusArj@users.noreply.github.com>
2021-11-14 04:17:21 +0100

    fixed

--- P2/P2/P2-E1/src/business/managers/EspectaculoManager.java
+++ P2/P2/P2-E1/src/business/managers/EspectaculoManager.java
@@ -152,12 +152,12 @@ public class EspectaculoManager
 		return requestedPunt;
 	}
 	
-	public Boolean createEspectaculoMult(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas, ArrayList<FechasDTO> fechas, int idf) {
+	public Boolean createEspectaculoMult(int id, String titulo, String categoria,String descripcion, int localidades_venta, int localidades_vendidas, ArrayList<FechasDTO> fechas) {
 		EspectaculoDAO newMultDAO = new EspectaculoDAO();
 		if(!this.MultExist(id)) 
 		{
 			EspectaculoMultDTO newMultDTO = new EspectaculoMultDTO(id, titulo, categoria, descripcion, localidades_venta, localidades_vendidas, fechas);  
-			newMultDAO.createEspectaculoMultiple(newMultDTO, idf);	
+			newMultDAO.createEspectaculoMultiple(newMultDTO);	
 			return true;
 		}
 		return false;
