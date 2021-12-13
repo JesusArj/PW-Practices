c4cff26744d6ad0d47f751764da9226e59f3079b
Antonio Lujano Luna <71549483+AntonioLujanoLuna@users.noreply.github.com>
2021-12-13 19:55:12 +0100

    a
    a

---
+++ P3/src/main/java/servlets/FilterServlet.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import display.javabean.CustomerBean;
+
+@WebFilter("/*")
+public abstract class FilterServlet implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+        throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+   		CustomerBean customerBean = (CustomerBean) session.getAttribute("customerBean");
+        String requestPath = request.getRequestURI();
+        if (needsAuthentication(requestPath) || customerBean == null || customerBean.getEmailUser().equals("")) {
+            	response.sendRedirect(request.getContextPath() + "/index.jsp"); // No logged-in user found, so redirect to login page.
+        } else {
+            chain.doFilter(req, res); // Logged-in user found, so just continue request.
+       	} 
+    }
+
+    //basic validation of pages that do not require authentication
+    private boolean needsAuthentication(String url) {
+        String[] validNonAuthenticationUrls =
+            { "loginView.jsp", "registerView.jsp", "index.jsp" };
+        for(String validUrl : validNonAuthenticationUrls) {
+            if (url.endsWith(validUrl)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
